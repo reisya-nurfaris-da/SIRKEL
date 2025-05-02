77f9967f1ad48e4da0596021c23ce8cbc46c727b
reisya-nurfaris-da <mipatigahhh@gmail.com>
2025-05-03 02:51:08 +0800

    database migration

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,7 @@
 class Constants {
-  static const String supabaseUrl = 'https://qfbqoznxsczffaeoklev.supabase.co';
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmYnFvem54c2N6ZmZhZW9rbGV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwMjk4ODAsImV4cCI6MjA2MTYwNTg4MH0.wUZbC-_pzcBPJGDr4NGvpJ9ss-vmXKx28N_fVBB9Lug';
+  static const String supabaseUrl = 'https://iwcfqrfrzwnxmvbfpydv.supabase.co';
+  static const String supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3Y2ZxcmZyendueG12YmZweWR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxODQzMzQsImV4cCI6MjA2MTc2MDMzNH0.9LswOnwjYM2WeqAHsWP6Z1aC963AYLdxGUOzBccTflY';
   static const String dateFormat = 'dd MMM yyyy';
   static const String timeFormat = 'HH:mm';
   static const String prefUserKey = 'user_data';
